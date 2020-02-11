27fe5ba2f659548726f9100cc0fbab9f54f0838a
Adrian Friedrich <adrifri@gmx.de>
2020-02-11 22:35:32 +0100

    initial project setting

---
+++ backend/src/main/java/adrian/friedrich/heizung/backend/App.java
@@ -0,0 +1,13 @@
+package adrian.friedrich.heizung.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
